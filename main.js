3bfb05a39f250451092ff420d590d05bee480a12
FelpsSantosx <lipesouzasanto@gmail.com>
2024-02-27 16:35:40 -0300

    first-commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Movimento para animação do titulo
+function alterarTexto() {
+    var titulo = document.getElementById("homePage__titulo__span");
+    if (titulo.innerHTML === " Desenvolvedor") {
+        titulo.innerHTML = " Front-End";
+    } else {
+        titulo.innerHTML = " Desenvolvedor";
+    }
+}
+
+setInterval(alterarTexto, 5000)
